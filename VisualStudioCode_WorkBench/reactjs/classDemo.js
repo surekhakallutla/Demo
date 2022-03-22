ed6f74c99b07fb927711f67a7a9de767ccf4a3bb
surekhakallutla <94508173+surekhakallutla@users.noreply.github.com>
2022-03-22 14:22:35 +0530

    Uploading JavaScript Demo files

---
+++ VisualStudioCode_WorkBench/reactjs/classDemo.js
@@ -0,0 +1,20 @@
+//creating a class
+class Demo {
+    //creating a constructor
+    constructor() {
+
+    }
+
+    //functions
+    myFunction1(parameters) {
+
+    }
+
+    myFunction2(parameters) {
+
+    }
+}
+
+//creating an object
+const obj = new Demo();
+obj.myFunction1();
